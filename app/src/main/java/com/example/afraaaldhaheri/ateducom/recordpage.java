81e5fc30ff0d6e56cde270773369c19c2b2cfb91
Sunkyoung <sun_k109@naver.com>
2018-02-24 23:56:11 +0400

    3nd commit

--- app/src/main/java/com/example/afraaaldhaheri/ateducom/recordpage.java
+++ app/src/main/java/com/example/afraaaldhaheri/ateducom/recordpage.java
@@ -2,18 +2,15 @@ package com.example.afraaaldhaheri.ateducom;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -24,7 +21,8 @@ public class recordpage extends AppCompatActivity {
     ImageView imageView;
 
 
-    private TextView sp2txt, corsename, instructurename;
+    private TextView sp2txt;
+
 
 
     @Override
@@ -32,12 +30,7 @@ public class recordpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordpage);
         sp2txt = (TextView) findViewById(R.id.sp2txt);
-        corsename = (TextView) findViewById( R.id.coursename );
-        instructurename = (TextView) findViewById( R.id.instructure );
-
-
         Button btnCamera = (Button) findViewById(R.id.camera);
-       // imageView = (ImageView)findViewById(R.id.imageView);
 
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
@@ -46,9 +39,9 @@ public class recordpage extends AppCompatActivity {
                 Intent intent = new Intent( MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent,0);
 
+
             }
         });
-
 
     }
 
@@ -56,10 +49,7 @@ public class recordpage extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
-
-        switch (requestCode) {
+       switch (requestCode) {
             case 10:
                 if (resultCode == RESULT_OK && data != null) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
@@ -67,6 +57,8 @@ public class recordpage extends AppCompatActivity {
                 }
                 break;
         }
+       // Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        // imageView.setImageBitmap(bitmap);
 
     }
 
@@ -82,6 +74,5 @@ public class recordpage extends AppCompatActivity {
             Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
         }
     }
-
 
 }
